298ee511abf839673dadb8b425c8de1c7706703b
buhama <hassan@hassanahmed.ca>
2022-01-06 16:04:23 -0500

    trying to fix the error

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Menu from './components/Menu';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
       <h2>Hello World</h2>
+      <Menu />
     </div>
   );
 }
---
+++ src/components/Menu.js
@@ -0,0 +1,9 @@
+const Menu = () => {
+  return (
+    <div>
+      <h2>yo what's good</h2>
+    </div>
+  );
+};
+
+export default Menu;
