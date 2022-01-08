21a3b419e97c987d7736c5eb2cc6e15f77a7f64d
buhama <hassan@hassanahmed.ca>
2022-01-07 19:40:12 -0500

    we got boxes on top of boxes

--- src/components/GettingStarted/PriceBlock.js
+++ src/components/GettingStarted/PriceBlock.js
@@ -1,5 +1,15 @@
+import { Box } from '@chakra-ui/react';
 const PriceBlock = () => {
-  return <div>hello world</div>;
+  return (
+    <div>
+      <Box bg='black' w='100%' p={4}>
+        WHAT"S GOOD
+      </Box>
+      <Box bg='gray.100' color='black' p={4}>
+        THIS IS AWESOME
+      </Box>
+    </div>
+  );
 };
 
 export default PriceBlock;
