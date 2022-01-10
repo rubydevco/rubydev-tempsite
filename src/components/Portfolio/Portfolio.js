fe4f5552d384e2fe03f853f6b75521021b082760
buhama <hassan@hassanahmed.ca>
2022-01-09 23:37:20 -0500

    added captions to portfolio

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -13,34 +13,43 @@ const Portfolio = () => {
     {
       img: thirtyfivemm,
       link: 'https://www.35mm.website/',
+      caption: 'A photo gallery of travelling through Vietnam',
     },
     {
       img: merci,
       link: 'https://www.mercicanada.org/',
+      caption: 'idk what this site does',
     },
     {
       img: thatmakergirl,
       link: 'https://www.thatmakergirl.ca/',
+      caption: 'Storefront bringing local handmade art ideas to life',
     },
     {
       img: designleague,
       link: 'https://www.uottawadesignleague.ca/',
+      caption: 'An event for a 3-day University of Ottawa student event',
     },
     {
       img: cvuo,
       link: 'https://cvuo.ca/',
+      caption: 'The massive clubs portal for the University of Ottawa',
     },
     {
       img: mess,
       link: 'https://mess-segm.ca/',
+      caption: 'The hub for the Mechanical Engineering Student Society',
     },
     {
       img: mpsp,
       link: 'https://uottawampsp.com/',
+      caption:
+        'A site for students aspiring for the opportunity to participate as members of Parliament at the House of Commons',
     },
     {
       img: ruby,
       link: 'https://uottawampsp.com/',
+      caption: 'A software development company',
     },
   ];
   return (
@@ -52,9 +61,14 @@ const Portfolio = () => {
           spacing={2}
         >
           {images.map((image) => (
-            <Link href={image.link} isExternal>
+            <Link
+              href={image.link}
+              isExternal
+              style={{ textDecoration: 'none' }}
+            >
               {' '}
               <Image loading='lazy' src={image.img} />
+              <p className='portfolio-image-caption'>{image.caption}</p>
             </Link>
           ))}
         </SimpleGrid>
