5a1701d976faaf308f84a82246291d6c37b8e18a
buhama <hassan@hassanahmed.ca>
2022-01-11 01:02:55 -0500

    added some things to the slider

--- src/components/GettingStarted/Slider.js
+++ src/components/GettingStarted/Slider.js
@@ -8,6 +8,8 @@ import {
   DrawerCloseButton,
   Input,
   Button,
+  Text,
+  Select,
 } from '@chakra-ui/react';
 
 const Slider = ({ isOpen, onClose, btnRef }) => {
@@ -25,7 +27,15 @@ const Slider = ({ isOpen, onClose, btnRef }) => {
           <DrawerHeader>Create your account</DrawerHeader>
 
           <DrawerBody>
+            <Text my={4}>Your Name</Text>
             <Input placeholder='Type here...' />
+            <Text my={4}>Your Email</Text>
+            <Input placeholder='Type here...' />
+            <Select my={4}>
+              <option value='option1'>Option 1</option>
+              <option value='option2'>Option 2</option>
+              <option value='option3'>Option 3</option>
+            </Select>
           </DrawerBody>
 
           <DrawerFooter>
