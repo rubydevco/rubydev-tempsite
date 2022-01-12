ef5bc95bf84b8cacea9436b4f86e230f0664412f
buhama <hassan@hassanahmed.ca>
2022-01-11 20:01:19 -0500

    moved grid to main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,6 @@
 import Menu from './Menu';
 import CanvasMenu from '../js/CanvasMenu';
+import { Grid, GridItem, Center, Box } from '@chakra-ui/react';
 
 const Main = () => {
   return (
@@ -7,7 +8,17 @@ const Main = () => {
       <div style={{ width: '100%', height: '100%', zIndex: '0' }}>
         <CanvasMenu />
       </div>
-      <Menu style={{ zIndex: '1' }} />
+      <section className='menu'>
+        <Grid templateColumns='repeat(10, 1fr)' gap={1}>
+          <GridItem colSpan={{ base: 10, md: 4, lg: 4 }}>
+            <Center m={4}>
+              <Box bg='black' w='70%' p={4} color='white'>
+                <Menu style={{ zIndex: '1' }} />
+              </Box>
+            </Center>
+          </GridItem>
+        </Grid>
+      </section>
     </>
   );
 };
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -13,43 +13,32 @@ const Menu = () => {
   }
 
   return (
-    <section className='menu'>
-      <Grid templateColumns='repeat(10, 1fr)' gap={1}>
-        <GridItem colSpan={{ base: 10, md: 4, lg: 4 }}>
-          <Center m={4}>
-            <Box bg='black' w='70%' p={4} color='white'>
-              <Link
-                as={RouterLink}
-                to='/getting-started'
-                style={{ textDecoration: 'none' }}
-              >
-                <MenuLink text='Get Started' />
-              </Link>
-              <Link
-                as={RouterLink}
-                to='/portfolio'
-                style={{ textDecoration: 'none' }}
-              >
-                <MenuLink text='Portfolio' />
-              </Link>
-              <Link
-                as={RouterLink}
-                to='/about-us'
-                style={{ textDecoration: 'none' }}
-              >
-                <MenuLink text='About Us' />
-              </Link>
-              <Link
-                onClick={toggleColorMode}
-                style={{ textDecoration: 'none' }}
-              >
-                <MenuLink text={darkModeText} />
-              </Link>
-            </Box>
-          </Center>
-        </GridItem>
-      </Grid>
-    </section>
+    <>
+      {/* <Grid templateColumns='repeat(10, 1fr)' gap={1}>
+        <GridItem colSpan={{ base: 10, md: 4, lg: 4 }}> */}
+      {/* <Center m={4}>
+        <Box bg='black' w='70%' p={4} color='white'> */}
+      <Link
+        as={RouterLink}
+        to='/getting-started'
+        style={{ textDecoration: 'none' }}
+      >
+        <MenuLink text='Get Started' />
+      </Link>
+      <Link as={RouterLink} to='/portfolio' style={{ textDecoration: 'none' }}>
+        <MenuLink text='Portfolio' />
+      </Link>
+      <Link as={RouterLink} to='/about-us' style={{ textDecoration: 'none' }}>
+        <MenuLink text='About Us' />
+      </Link>
+      <Link onClick={toggleColorMode} style={{ textDecoration: 'none' }}>
+        <MenuLink text={darkModeText} />
+      </Link>
+      {/* </Box>
+      </Center> */}
+      {/* </GridItem>
+      </Grid> */}
+    </>
   );
 };
 
