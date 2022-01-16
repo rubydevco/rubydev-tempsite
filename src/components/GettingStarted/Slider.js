ee4d0bf3b0d87593358a76db5da72be3008205eb
buhama <hassan@hassanahmed.ca>
2022-01-16 00:17:06 -0500

    added most slider features and connected form to google sheet

--- src/components/GettingStarted/Slider.js
+++ src/components/GettingStarted/Slider.js
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+import axios from 'axios';
+import { SHEETS_KEY } from '../../secret';
 import {
   Drawer,
   DrawerBody,
@@ -8,16 +11,37 @@ import {
   DrawerCloseButton,
   Input,
   Button,
-  Text,
   Select,
+  FormControl,
+  FormLabel,
+  Textarea,
 } from '@chakra-ui/react';
 
 const Slider = ({ isOpen, onClose, btnRef, type }) => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [selectedType, setSelectedType] = useState(type.title);
+  const [comments, setComments] = useState('');
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const newSubmission = {
+      name: name,
+      email: email,
+      selectedType: selectedType,
+      comments: comments,
+    };
+
+    axios.post(SHEETS_KEY, newSubmission).then((response) => {
+      console.log(response);
+      onClose();
+    });
+  };
   return (
     <>
       <Drawer
         isOpen={isOpen}
-        placement='right'
+        placement='left'
         onClose={onClose}
         finalFocusRef={btnRef}
       >
@@ -36,30 +60,58 @@ const Slider = ({ isOpen, onClose, btnRef, type }) => {
             >
               {type.description}
             </h2>
-            <Text my={4}>Your Name</Text>
-            <Input placeholder='Type here...' />
-            <Text my={4}>Your Email</Text>
-            <Input placeholder='Type here...' />
-            <Select my={4}>
-              <option value={type.title}>{type.title}</option>
-              <option
-                value={type.title === 'PROFESSIONAL' ? 'BASIC' : 'PROFESSIONAL'}
-              >
-                {type.title === 'PROFESSIONAL' ? 'BASIC' : 'PROFESSIONAL'}
-              </option>
-              <option
-                value={type.title === 'E-COMMERCE' ? 'BASIC' : 'E-COMMERCE'}
-              >
-                {type.title === 'E-COMMERCE' ? 'BASIC' : 'E-COMMERCE'}
-              </option>
-            </Select>
+            <form id='form' method='POST' onSubmit={onSubmit}>
+              <FormControl isRequired>
+                <FormLabel my={4}>Your Name</FormLabel>
+                <Input
+                  placeholder='Type here...'
+                  onChange={(e) => setName(e.target.value)}
+                  id='name'
+                  name='name'
+                />
+              </FormControl>
+              <FormControl isRequired>
+                <FormLabel my={4}>Your Email</FormLabel>
+                <Input
+                  placeholder='Type here...'
+                  type='email'
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </FormControl>
+              <FormControl isRequired>
+                <FormLabel my={4}>The Type of Website</FormLabel>
+                <Select onChange={(e) => setSelectedType(e.target.value)}>
+                  <option value={type.title}>{type.title}</option>
+                  <option
+                    value={
+                      type.title === 'PROFESSIONAL' ? 'BASIC' : 'PROFESSIONAL'
+                    }
+                  >
+                    {type.title === 'PROFESSIONAL' ? 'BASIC' : 'PROFESSIONAL'}
+                  </option>
+                  <option
+                    value={type.title === 'E-COMMERCE' ? 'BASIC' : 'E-COMMERCE'}
+                  >
+                    {type.title === 'E-COMMERCE' ? 'BASIC' : 'E-COMMERCE'}
+                  </option>
+                  <option value='Not Sure'>Note Sure</option>
+                </Select>
+              </FormControl>
+              <FormLabel my={4}>Other Comments</FormLabel>
+              <Textarea
+                placeholder='Type here..'
+                onChange={(e) => setComments(e.target.value)}
+              ></Textarea>
+            </form>
           </DrawerBody>
 
           <DrawerFooter>
             <Button variant='outline' mr={3} onClick={onClose}>
               Cancel
             </Button>
-            <Button colorScheme='blue'>Save</Button>
+            <Button type='submit' form='form' colorScheme='blue'>
+              Send
+            </Button>
           </DrawerFooter>
         </DrawerContent>
       </Drawer>
