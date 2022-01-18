46f131a3b56b5b0ef0ee673fdeee5c707efc66c7
buhama <hassan@hassanahmed.ca>
2022-01-17 21:37:26 -0500

    github being wack af

--- src/components/Menu/MenuFooter.js
+++ src/components/Menu/MenuFooter.js
@@ -1,5 +1,46 @@
+import { Link } from '@chakra-ui/react';
+import { IconButton } from '@chakra-ui/react';
+import { FaInstagram, FaFacebookF, FaTwitter } from 'react-icons/fa';
+
 const MenuFooter = () => {
-  return <div>This is the footer</div>;
+  return (
+    <div className='footer'>
+      <Link
+        href='https://www.instagram.com/onyxonlinehq/'
+        isExternal
+        pr={2}
+        className='social'
+      >
+        <IconButton
+          size='sm'
+          aria-label='Instagram Link'
+          icon={<FaInstagram />}
+        />
+      </Link>
+
+      <Link
+        href='https://www.facebook.com/ONYXOnlineHQ/'
+        isExternal
+        pr={2}
+        className='social'
+      >
+        <IconButton
+          size='sm'
+          aria-label='Facebook Link'
+          icon={<FaFacebookF />}
+        />
+      </Link>
+
+      <Link
+        href='https://twitter.com/onyxonlinehq'
+        isExternal
+        pr={2}
+        className='social'
+      >
+        <IconButton size='sm' aria-label='Twitter Link' icon={<FaTwitter />} />
+      </Link>
+    </div>
+  );
 };
 
 export default MenuFooter;
