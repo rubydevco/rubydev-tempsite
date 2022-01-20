3ff6fbd982e7b7cde79986da17ad5751acf0668c
buhama <hassan@hassanahmed.ca>
2022-01-20 17:49:35 -0500

    Co-authored-by: Evan <BiigFish@users.noreply.github.com>

--- src/components/Menu/MenuFooter.js
+++ src/components/Menu/MenuFooter.js
@@ -1,54 +1,57 @@
-import { Link } from '@chakra-ui/react';
-import { IconButton } from '@chakra-ui/react';
+import { Link, IconButton, Box, Center } from '@chakra-ui/react';
 import { FaInstagram, FaFacebookF, FaTwitter } from 'react-icons/fa';
 
 const MenuFooter = () => {
   return (
     <div className='footer'>
-      <Link
-        href='https://www.instagram.com/onyxonlinehq/'
-        isExternal
-        pr={2}
-        className='social'
-      >
-        <IconButton
-          size='sm'
-          aria-label='Instagram Link'
-          icon={<FaInstagram />}
-          bg='gray.800'
-          color='white'
-        />
-      </Link>
+      <Box mt='4'>
+        <Center>
+          <Link
+            href='https://www.instagram.com/onyxonlinehq/'
+            isExternal
+            pr={2}
+            className='social'
+          >
+            <IconButton
+              size='md'
+              aria-label='Instagram Link'
+              icon={<FaInstagram />}
+              bg='gray.800'
+              color='white'
+            />
+          </Link>
 
-      <Link
-        href='https://www.facebook.com/ONYXOnlineHQ/'
-        isExternal
-        pr={2}
-        className='social'
-      >
-        <IconButton
-          size='sm'
-          aria-label='Facebook Link'
-          icon={<FaFacebookF />}
-          bg='gray.800'
-          color='white'
-        />
-      </Link>
+          <Link
+            href='https://www.facebook.com/ONYXOnlineHQ/'
+            isExternal
+            pr={2}
+            className='social'
+          >
+            <IconButton
+              size='md'
+              aria-label='Facebook Link'
+              icon={<FaFacebookF />}
+              bg='gray.800'
+              color='white'
+            />
+          </Link>
 
-      <Link
-        href='https://twitter.com/onyxonlinehq'
-        isExternal
-        pr={2}
-        className='social'
-      >
-        <IconButton
-          size='sm'
-          aria-label='Twitter Link'
-          icon={<FaTwitter />}
-          bg='gray.800'
-          color='white'
-        />
-      </Link>
+          <Link
+            href='https://twitter.com/onyxonlinehq'
+            isExternal
+            pr={2}
+            className='social'
+          >
+            <IconButton
+              size='md'
+              aria-label='Twitter Link'
+              icon={<FaTwitter />}
+              bg='gray.800'
+              color='white'
+            />
+          </Link>
+        </Center>
+      </Box>
     </div>
   );
 };
